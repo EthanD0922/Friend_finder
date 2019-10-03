b0e57b7b256d2d1ac9cad6b7650647ea67c1f222
Ethan <allric0922@gmail.com>
2019-10-02 23:23:41 -0400

    core of the best friend javascript added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,35 @@
-var example =[ {
-    test: 1234,
-    two_test: 5678,
-}
-]
-module.exports = example
+var friendsData = [
+    {
+        name: "tester",
+        picture: "blah",
+        scores: [ 
+            1,
+            2,
+            3,
+            4,
+            5,
+            4,
+            3,
+            2,
+            1,
+            5,
+        ]
+    },
+    {
+        name: "wrong guy",
+        picture: "blah",
+        scores: [ 
+            4,
+            2,
+            3,
+            4,
+            5,
+            4,
+            3,
+            2,
+            1,
+            5,
+        ]
+    }
+];
+module.exports = friendsData
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,5 +1,9 @@
 var friendsData = require("../data/friends");
 
+var totalScore = 0
+var bestScore = 100
+var bestFriendID = 0
+
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res){
@@ -7,8 +11,33 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res){
+        totalScore = 0;
+        bestScore = 100;
+        bestFriendID = 0;
+        for (i = 0; i < req.body.scores.length; i ++){
+            req.body.scores[i] = parseInt(req.body.scores[i])
+        }
+        newFriendsMatch(req.body)
+
         friendsData.push(req.body)
-        return res.json(req.body)
+        // res.send({
+        //     bestFriendName: friendsData[bestFriendID].name,
+        //     bestFriendPic: friendsData[bestFriendID].picture
+        // });
     })
 
+}
+
+function newFriendsMatch(newFriend){
+        for (i = 0; i < friendsData.length; i ++){
+            var j = 0
+            totalScore = 0;
+            for (j = 0; j < friendsData[i].scores.length; j ++){
+                totalScore += Math.abs(friendsData[i].scores[j] - newFriend.scores[j])
+            }
+            if (j === 10 && totalScore < bestScore){
+                bestScore = totalScore;
+                bestFriendID = i;
+            }
+        }
 }
